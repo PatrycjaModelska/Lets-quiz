ff669a6982f33eb9c5abce8cc2f13c4f8243daa4
Modelka <patrycja.modelska@onet.pl>
2021-03-30 21:41:23 +0200

    Custom authentication backend added

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -104,6 +104,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+)
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
